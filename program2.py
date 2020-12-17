94440a409f065fb5d672c2c5e8a163086f7c92a0
Jason <68907860+JJ-REDRAGON@users.noreply.github.com>
2020-12-17 15:42:17 +0400

    variables and user input
    this program asks the users to enter number and strings and then prints the modified result

---
+++ program2.py
@@ -0,0 +1,23 @@
+a = 2
+b = 4
+c = 8
+print(a+b+c)
+
+d = input('enter a number : ')
+d = int(d)
+print(d+2)
+
+#we will take 3 numbers as input from user
+#we will add these 3 numbers
+#we will print the result
+a = input('enter a : ')
+a = int(a)
+b = input('enter b : ')
+b = int(b)
+c = input('enter c : ')
+c = int(c)
+d = a + b + c
+print(d)
+
+name = input('enter your name : ')
+print('Hello ' + name)
