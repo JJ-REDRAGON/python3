27b34bac66b715886cf3803ddded570c3e7bba5d
Jason <68907860+JJ-REDRAGON@users.noreply.github.com>
2020-12-22 16:24:17 +0400

    for loops practice
    learnt how to create a nested for loop

---
+++ program4.py
@@ -0,0 +1,62 @@
+#for loops practice
+
+for count in range(1,10+1):
+    print(count)
+
+print('---')
+
+for a in range(10,0,-1):
+    print(a)
+
+print('---')
+
+for b in range(2,11,2):
+    print(b)
+
+print('---')
+
+for b in range(3,31,3):
+    print(b)
+
+print('---')
+
+for c in range(7,70+1,7):
+    print(c)
+
+print('---')
+
+for d in range(80,8-1,-8):
+    print(d)
+
+print('---')
+
+for i in range(1,10+1,1):
+    print(7*i)
+
+print('---')
+
+for x in range(1,11,1):
+    print(9*x)
+
+print('---')
+
+#practice problems
+
+'''
+Exercise Question 1: Print First 10 natural numbers using while loop
+'''
+
+for x in range(0,11):
+    print(x)
+
+print('---')
+
+for i in range(1,3+1):
+    print('***')
+
+print('---')
+
+for i in range(1,3+1):
+    for j in range(1,3+1):
+        print('*',end=' ')
+    print(' ',end='\n')
