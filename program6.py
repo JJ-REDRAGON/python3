e4d69fe44c154c28474b5f2af8ac4e1291b06c4c
Jason <68907860+JJ-REDRAGON@users.noreply.github.com>
2021-01-05 16:18:36 +0400

    Python data types and for loops
    playing around with python data types and for loops and learning them more.

---
+++ program6.py
@@ -0,0 +1,42 @@
+a = 5
+
+print(type(a))
+
+b = 5.5
+
+print(type(b))
+
+c = 5.1111_1111_1111_1111_3
+
+print(c)
+
+print('---')
+
+d = [1.1,2.2,3.3,4.4,'python',5.5]
+
+print(d)
+print(d[1])
+print(d[3])
+print(d[5])
+print(d[4])
+
+print('---')
+
+friends = ['John Doe','skeppy','Jason','Dev','Jack']
+
+print(friends[3])
+print(friends[4])
+print(friends[0])
+
+print('---')
+
+for i in range(0,5):
+    print(friends[i])
+    
+print('---')
+
+months = ['January','Febuary','March','April','May','June','July','August','september','october','November','December']
+
+for x in range(0,12):
+    print(months[x])
+
