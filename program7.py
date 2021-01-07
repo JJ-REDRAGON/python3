5c46a9c599dc9346b476b95b506cf03983257189
Jason <68907860+JJ-REDRAGON@users.noreply.github.com>
2021-01-07 16:23:25 +0400

    For loop extraction
    we have made multiple lists of days, months, rich people etc. for - loops to learn and understand more about it.

---
+++ program7.py
@@ -0,0 +1,53 @@
+weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+for a in range(0,7):
+    print(weekdays[a])
+
+print('---')
+
+Zodiac = ['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius','Pisces']
+for a in range(0,12):
+    print(Zodiac[a])
+
+print('---')
+
+Rainbow = ['Red','Orange','Yellow','Green','Light Blue','Blue','Purple']
+for a in range(0,7):
+    print(Rainbow[a])
+
+print('---')
+
+Top5rich = ['Jeff Bezos','Elon Musk','Bernard Arnault','Bill Gates','Mark Zuckerberg']
+for a in range(0,5):
+    print(Top5rich[a])
+
+print('---')
+
+Top5richW = ['Alice Walton','Francoise Bettencourt','Julia Koch','Mackenzie Bezos','Jacqueline Mars']
+for a in range(0,5):
+    print(Top5richW[a])
+
+print('---')
+
+for a in range(5,7):
+    print(weekdays[a])
+
+print('---')
+
+for a in range(0,3):
+    print(Top5rich[a])
+
+print('---')
+
+for a in range(4,7):
+    print(Rainbow[a])
+
+print('---')
+
+months = ['January','Febuary','March','April','May','June','July','August','september','october','November','December']
+for a in range(7,11):
+    print(months[a])
+
+print('---')
+
+for a in range(0,5):
+    print(weekdays[a])
